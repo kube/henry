8a37520b034ffa31144928ca5c762e08ff246934
Christophe Feijoo <christophe.feijoo@teads.tv>
2017-09-15 18:16:03 +0200

    Update

---
+++ mocks/tag.js
@@ -0,0 +1,35 @@
+;(function(w, d, s) {
+  try {
+    d = w.top.document || d
+    w = w.top.document ? w.top : w
+  } catch (e) {}
+
+  var ttag = function() {
+    w.teads
+      .page(15743)
+      .placement(27829, {
+        css: 'padding: 13px 0px 0px 16px;',
+        format: 'inboard',
+        parentWindow: false,
+        slot: {
+          insertBefore: true,
+          btf: true,
+          selector: 'p',
+          minimum: 1
+        }
+      })
+      .serve()
+  }
+
+  if (w.teads && w.teads.page) {
+    ttag()
+  } else if (!w.teadsscript) {
+    s.src =
+      '//cdn.teads.tv/media/format/v3/teads-format.min.js'
+    s.async = true
+    s.onload = ttag
+    w.teadsscript = d
+      .getElementsByTagName('head')[0]
+      .appendChild(s)
+  }
+})(window, document, document.createElement('script'))
--- src/index.ts
+++ src/index.ts
@@ -1,98 +1,81 @@
-import { readFileSync } from 'fs'
+import { createReadStream } from 'fs'
 import { join } from 'path'
 import * as http from 'http'
-import * as https from 'https'
 import * as httpProxy from 'http-proxy'
 
 import { Url, parse as parseUrl } from 'url'
-import { parse as parseQueryString } from 'querystring'
 
-// const enableHttpsProxy = true
 const HTTP_PORT = 4242
-const HTTPS_PORT = 7345
-
 const proxy = httpProxy.createProxyServer({})
 
-const isTeadsRequestUrl = ({ host }: Url) => /teads\.tv$/.test(host!)
+const isTeadsRequestUrl = (url: Url) =>
+  /teads\.tv$/.test(url.host!)
+
+// const isFormatFrameworkRequestUrl = (url: Url) =>
+//   isTeadsRequestUrl(url) &&
+//     /^/.test(url.path!)
+
+const isTagRequestUrl = (url: Url) =>
+  isTeadsRequestUrl(url) &&
+  /^\/page\/[0-9]+\/tag/.test(url.path!)
 
 const isAdRequestUrl = (url: Url) =>
-  isTeadsRequestUrl(url) && /^\/page\/[0-9]+\/ad/.test(url.path!)
+  isTeadsRequestUrl(url) &&
+  /^\/page\/[0-9]+\/ad/.test(url.path!)
 
 const getTarget = ({ protocol, host, port }: Url) => {
-  const target = protocol + '//' + host + (port ? ':' + port : '')
+  const target = /:/.test(host!)
+    ? protocol + '//' + host
+    : protocol + '//' + host + (port ? ':' + port : '')
   return target
 }
 
 http
   .createServer((req, res) => {
     if (req.url) {
-      console.log('HTTP REQUEST')
       const url = parseUrl(req.url)
 
-      if (isAdRequestUrl(url))
-        console.log(JSON.parse(parseQueryString(url.query).page))
-
-      proxy.web(
-        req,
-        res,
-        {
-          target: getTarget(url)
-        },
-        err => {
-          if (err) {
-            console.error(err)
-          }
-          res.end('Sorry.')
-        }
+      res.setHeader(
+        'Access-Control-Allow-Credentials',
+        'true'
       )
+      res.setHeader(
+        'Access-Control-Allow-Origin',
+        getTarget(parseUrl('' + req.headers.referer))
+      )
+
+      if (isTagRequestUrl(url))
+        console.log(':: TAG REQUEST URL') ||
+          createReadStream(
+            join(__dirname, '../mocks/tag.js')
+          ).pipe(res)
+      else if (isAdRequestUrl(url))
+        console.log(':: AD REQUEST URL') ||
+          createReadStream(
+            join(__dirname, '../mocks/ad.json')
+          ).pipe(res)
+      else
+        console.log(getTarget(url)) ||
+          proxy.web(
+            req,
+            res,
+            {
+              target: getTarget(url)
+            },
+            err => {
+              console.error(err)
+              res.end('Sorry.')
+            }
+          )
     } else {
       res.end('No url')
     }
   })
   .listen(HTTP_PORT, (err: any) => {
-    if (err) console.error('Could not create HTTP proxy server')
-    else console.log(`HTTP Proxy Server listening on ${HTTP_PORT}`)
-  })
-
-httpProxy
-  .createServer({
-    target: '127.0.0.1:4242',
-    ssl: {
-      key: readFileSync(join(__dirname, '../certificates/key.pem')),
-      cert: readFileSync(join(__dirname, '../certificates/cert.pem')),
-      passphrase: 'bonjourtoutlemonde'
-    },
-    secure: false
-  })
-  .listen(4243)
-
-https
-  .createServer(
-    {
-      key: readFileSync(join(__dirname, '../certificates/key.pem')),
-      cert: readFileSync(join(__dirname, '../certificates/cert.pem')),
-      passphrase: 'bonjourtoutlemonde'
-    },
-    (req, res) => {
-      console.log('HTTPS')
-      console.log(req.url)
-
-      proxy.web(
-        req,
-        res,
-        {
-          target: req.url
-        },
-        err => {
-          if (err) {
-            console.error(err)
-          }
-          res.end('Sorry.')
-        }
+    if (err)
+      console.error('Could not create HTTP proxy server')
+    else
+      console.log(
+        `HTTP Proxy Server listening on ${HTTP_PORT}`
       )
-    }
-  )
-  .listen(HTTPS_PORT, (err: any) => {
-    if (err) console.error('Could not create HTTPS proxy server')
-    else console.log(`HTTPS Proxy Server listening on ${HTTPS_PORT}`)
   })
